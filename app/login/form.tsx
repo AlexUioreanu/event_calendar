f412a703a4583f9014133099a601e2d91ea12119
alexandru.uioreanu <alexandru.uioreanu@accesa.eu>
2024-03-25 14:45:23 +0200

    finalize login

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import React, { FormEvent, useState } from "react";
 import Link from "@mui/material/Link";
-import { whiteButtonOutlineStyles } from "@/utils";
+import { whiteButtonOutlineStyles } from "../utils";
 
 export default function Form() {
   const router = useRouter();
@@ -45,77 +45,73 @@ export default function Form() {
 
   return (
     <div className="signInSignUpContainer">
-      <Image
-        className="mb-56"
-        width={300}
-        height={300}
-        src="/logo.svg"
-        alt="Logo"
-      />
-
       <div
         style={{
-          color: "white",
-          textAlign: "center",
+          maxWidth: "500px",
+          margin: "auto",
+          marginTop: "auto",
+          padding: "20px",
+          borderRadius: "2rem",
+          backgroundColor: "#fff",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        <h1
-          style={{ fontSize: "48px", fontWeight: "bold", marginBottom: "20px" }}
-        >
-          Welcome
-        </h1>
-        <p style={{ fontSize: "24px", fontWeight: "initial" }}>
-          Please enter your credentials
-        </p>
-      </div>
-
-      <form onSubmit={handleSubmit} className="flex flex-col gap-2 w-96 mt-10 ">
-        <OutlinedTextField
-          name="email"
-          label="Email"
-          type="email"
-          onChange={handleChange}
-          value={inputs.email}
-          required
-          sx={whiteButtonOutlineStyles}
-        />
-        <OutlinedTextField
-          name="password"
-          label="Password"
-          type="password"
-          onChange={handleChange}
-          value={inputs.password}
-          required
-          sx={whiteButtonOutlineStyles}
-        />
-        <button
-          type="submit"
+        <div
           style={{
-            backgroundColor: "#FFA500",
-            color: "white",
-            padding: "10px 20px",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
-            fontSize: "16px",
-            fontWeight: "bold",
-            outline: "none",
+            color: "black",
+            textAlign: "center",
           }}
         >
-          Login
-        </button>
-        <Link
-          href="/register"
-          sx={{
-            textDecoration: "none",
-            color: "#056bf0",
-            fontWeight: "normal",
-            marginBottom: "50px",
-          }}
+          <h1
+            style={{
+              fontSize: "48px",
+              fontWeight: "bold",
+              marginBottom: "20px",
+            }}
+          >
+            Welcome
+          </h1>
+          <p style={{ fontSize: "24px", fontWeight: "initial" }}>
+            Please enter your credentials
+          </p>
+        </div>
+
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-2 w-96 mt-10 "
         >
-          You don't have an account? SignUp
-        </Link>
-      </form>
+          <OutlinedTextField
+            name="email"
+            label="Email"
+            type="email"
+            onChange={handleChange}
+            value={inputs.email}
+            required
+            sx={whiteButtonOutlineStyles}
+          />
+          <OutlinedTextField
+            name="password"
+            label="Password"
+            type="password"
+            onChange={handleChange}
+            value={inputs.password}
+            required
+            sx={whiteButtonOutlineStyles}
+          />
+          <button
+            type="submit"
+            className="loginButton"
+            style={{
+              marginTop: "1rem",
+            }}
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ app/utils/index.ts
@@ -0,0 +1,33 @@
+export const whiteButtonOutlineStyles = {
+  "& label.Mui-focused": {
+    color: "#A66914",
+  },
+  "& label": {
+    color: "#A66914",
+  },
+  "& .MuiOutlinedInput-root": {
+    "&.Mui-focused fieldset": {
+      borderColor: "transparent",
+    },
+    "&:hover fieldset": {
+      borderColor: "transparent",
+    },
+    "& fieldset": {
+      borderColor: "transparent",
+    },
+    backgroundColor: "#F3F3F3",
+    borderRadius: "25px",
+  },
+  "& .MuiOutlinedInput-input": {
+    color: "#A66914",
+  },
+  "& .MuiInput-underline:before": {
+    borderBottom: "1px solid #A66914",
+  },
+  "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+    borderBottom: "2px solid #A66914",
+  },
+  "& .MuiInput-underline:after": {
+    borderBottom: "2px solid #A66914",
+  },
+};
